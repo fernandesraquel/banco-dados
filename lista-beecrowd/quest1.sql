be28b843ad4d7f48a4ab5127eac97c50e1eb74b2
Raquel Fernandes <94188045+fernandesraquel@users.noreply.github.com>
2022-12-06 14:47:49 -0300

    Update quest1.sql

--- lista-beecrowd/quest1.sql
+++ lista-beecrowd/quest1.sql
@@ -8,29 +8,14 @@ CREATE TABLE products (
 
 # Carga inicial
 
-insert into products
-(id, name, amount, price)
-values (1, 'Two-door wardrobe', 100, 80);
+insert into products (id, name, amount, price) values 
+(1, 'Two-door wardrobe', 100, 80),
+(2, 'Dining table', 1000, 560),
+(3, 'Towel holder', 10000, 5.50),
+(4, 'Computer desk', 350, 100),
+(5, 'Chair', 3000, 210.64),
+(6, 'Single bed', 750, 99);
 
-insert into products
-(id, name, amount, price)
-values (2, 'Dining table', 1000, 560);
-
-insert into products
-(id, name, amount, price)
-values (3, 'Towel holder', 10000, 5.50);
-
-insert into products
-(id, name, amount, price)
-values (4, 'Computer desk', 350, 100);
-
-insert into products
-(id, name, amount, price)
-values (5, 'Chair', 3000, 210.64);
-
-insert into products
-(id, name, amount, price)
-values (6, 'Single bed', 750, 99);
 
 /*SELECT id, name FROM products 
-WHERE price < 10 OR price > 100*/
+WHERE price < 10 OR price > 100 */
