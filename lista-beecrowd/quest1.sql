0880fdf7414c519513f0dedde5c9c1c98e66db28
Raquel Fernandes <94188045+fernandesraquel@users.noreply.github.com>
2022-12-08 09:36:04 -0300

    Add files via upload

--- lista-beecrowd/quest1.sql
+++ lista-beecrowd/quest1.sql
@@ -1,21 +1,15 @@
 CREATE TABLE products (
- 	id INTEGER NOT NULL,
- 	name varchar(100) NOT NULL,
-  	amount INTEGER DEFAULT NULL,
-  	price INTEGER DEFAULT NULL,
- 	PRIMARY KEY (id)	
+  id NUMERIC PRIMARY KEY,
+  name CHARACTER VARYING (255),
+  amount NUMERIC,
+  price NUMERIC
 );
 
-# Carga inicial
-
-insert into products (id, name, amount, price) values 
-(1, 'Two-door wardrobe', 100, 80),
-(2, 'Dining table', 1000, 560),
-(3, 'Towel holder', 10000, 5.50),
-(4, 'Computer desk', 350, 100),
-(5, 'Chair', 3000, 210.64),
-(6, 'Single bed', 750, 99);
-
-
-/*SELECT id, name FROM products 
-WHERE price < 10 OR price > 100 */
+INSERT INTO products (id, name, amount, price)
+VALUES 
+  (1,	'Two-door wardrobe',	100,	80),
+  (2,	'Dining table',	1000,	560),
+  (3,	'Towel holder',	10000,	5.50),
+  (4,	'Computer desk',	350,	100),
+  (5,	'Chair',	3000,	210.64),
+  (6,	'Single bed',	750,	99);
